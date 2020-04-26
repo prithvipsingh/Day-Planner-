46c755569811784fc696fb2e9758275ed815a989
prithvipsingh <ppsdeora@gmail.com>
2020-04-26 14:29:07 +1000

    update index.html, update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,45 @@
-//var m = moment();
-//console.log(m.toString());
+
+// gets data for the header date
+function getHeaderDate() {
+  var currentHeaderDate = moment().format('dddd, MMMM Do');
+  $("#currentDay").text(currentHeaderDate);
+}
+getHeaderDate();
+
 
 
 $(document).ready(function () {
-  
   // create hours variable in arrray 
 
-  var hours = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
-   // FOR LOOP FOR iterate 
-   for(var i =0; i <hours.length; i++){
+  var myHours = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
    
   
 
-   }
+   myHours.forEach(function(myHours) {
 
+    if (myHours < moment().format("HH")) {
+      planData.attr ({
+          "class": "past", 
+      })
+  } else if (myHours === moment().format("HH")) {
+      planData.attr({
+          "class": "present"
+      })
+  } else if (myHours > moment().format("HH")) {
+      planData.attr({
+          "class": "future"
+      })
+  }
 
+  // creates save button
+  var saveButton = $("<i class='far fa-save fa-lg'></i>")
+  var savePlan = $("<button>")
+      .attr({
+          "class": "col-md-1 saveBtn"
+  });
 
+})
+
+   });
 
 });
