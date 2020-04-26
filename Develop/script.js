8d795a2e0f5df674971c1c281411e2be94ea2e84
prithvipsingh <ppsdeora@gmail.com>
2020-04-26 12:30:21 +1000

    uploading all files

---
+++ Develop/script.js
@@ -0,0 +1,20 @@
+//var m = moment();
+//console.log(m.toString());
+
+
+$(document).ready(function () {
+  
+  // create hours variable in arrray 
+
+  var hours = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
+   // FOR LOOP FOR iterate 
+   for(var i =0; i <hours.length; i++){
+   
+  
+
+   }
+
+
+
+
+});
