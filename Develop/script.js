b73771ec63f3468352298a5518e3f400dbd2e137
prithvipsingh <ppsdeora@gmail.com>
2020-04-27 20:07:41 +1000

    adding event click function

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,30 @@ function getHeaderDate() {
   var currentHeaderDate = moment().format('dddd, MMMM Do');
   $("#currentDay").text(currentHeaderDate);
 }
+
+
+
+function saveReminders() {
+  localStorage.setItem("myHoursId", JSON.stringify(myHoursId));
+}
+
+// sets any data in localStorage to the view
+function displayReminders() {
+  myHoursId.forEach(function (myHour) {
+      $(`#${myHour}`).val;
+  })
+}
+
+// sets any existing localStorage data to the view if it exists
+function init() {
+  var storedDay = JSON.parse(localStorage.getItem("myHoursId"));
+  if (storedDay) {
+    myHoursId = storedDay;
+  }
+  saveReminders();
+  displayReminders();
+}
+
 getHeaderDate();
 
 
@@ -26,7 +50,24 @@ $(document).ready(function () {
     }
 
     // creates save button
+    var task = $("textarea");
+
     var saveButton = $("<i class='far fa-save fa-lg'></i>")
     var savePlan = $("<button>").attr("class","col-md-1 saveBtn");
     });
+  
+    //saves data to be used in localStorage
+    $(".saveBtn").on("click", function(event) {
+        event.preventDefault();
+        var saveIndex = $(this).siblings(".description").children(".future").attr("id");
+        myHoursId[saveIndex] = $(this).siblings(".description").children(".future").val();
+        console.log(saveIndex);
+        saveReminders();
+        displayReminders();
+    })
+
+
+
+
+
 })
